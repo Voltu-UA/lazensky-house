4f42fb194e3a62427836b5e3ef9ee2f9506f74ff
Sergey <s_shishka@ukr.net>
2025-10-26 12:46:59 +0000

    Adjusted gallery

--- public/main.js
+++ public/main.js
@@ -31,3 +31,36 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const btn = document.getElementById('showMoreBtn');
+//   const hiddenImages = document.querySelectorAll('.hidden-image');
+
+//   btn.addEventListener('click', () => {
+//     const isHidden = getComputedStyle(hiddenImages[0]).display === 'none';
+
+//     hiddenImages.forEach(img => {
+//       img.style.display = isHidden ? 'inline-block' : 'none';
+//     });
+
+//     btn.textContent = isHidden ? 'Приховати' : 'Показати більше';
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.getElementById('showMoreBtn');
+  const hiddenImages = document.querySelectorAll('.hidden-image');
+
+  let isHidden = true;
+
+  btn.addEventListener('click', () => {
+    hiddenImages.forEach(img => {
+      img.classList.toggle('show');
+    });
+
+    isHidden = !isHidden;
+    btn.textContent = isHidden ? 'Показати більше' : 'Приховати';
+  });
+});
+
