3ef6a91279225359be57b63d3e07ed90a759f3aa
Sergey <s_shishka@ukr.net>
2025-11-11 16:08:18 +0000

    Added video, youtube links, plans

--- public/main.js
+++ public/main.js
@@ -1,36 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const form = document.getElementById('contact-form');
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+//   form.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-    const formData = {
-      name: form.elements['name'].value,
-      email: form.elements['email'].value,
-      message: form.elements['message'].value
-    };
+//     const formData = {
+//       name: form.elements['name'].value,
+//       email: form.elements['email'].value,
+//       message: form.elements['message'].value
+//     };
 
-    fetch('/contacts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    })
-    .then(response => {
-      if (response.ok) {
-        alert('Дякуємо! Ваше повідомлення надіслано.');
-        form.reset();
-      } else {
-        alert('Помилка при надсиланні. Спробуйте ще раз.');
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      alert('Не вдалося надіслати повідомлення.');
-    });
-  });
-});
+//     fetch('/contacts', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(formData)
+//     })
+//     .then(response => {
+//       if (response.ok) {
+//         alert('Дякуємо! Ваше повідомлення надіслано.');
+//         form.reset();
+//       } else {
+//         alert('Помилка при надсиланні. Спробуйте ще раз.');
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error:', error);
+//       alert('Не вдалося надіслати повідомлення.');
+//     });
+//   });
+// });
 
 
 // document.addEventListener('DOMContentLoaded', function () {
@@ -64,3 +64,32 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.getElementById('showMorePlansBtn');
+  const hiddenPlans = document.querySelectorAll('#plans .hidden-image');
+
+  let isHidden = true;
+
+  btn.addEventListener('click', () => {
+    hiddenPlans.forEach(img => {
+      img.classList.toggle('show');
+    });
+
+    isHidden = !isHidden;
+    btn.textContent = isHidden ? 'Показати більше' : 'Приховати';
+  });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.history.scrollRestoration = 'manual';
+  if (window.location.hash) {
+    history.replaceState(null, null, ' ');
+  }
+  window.scrollTo(0, 0);
+});
+
+// document.getElementById('showMoreConstructionBtn').addEventListener('click', function () {
+//   document.querySelectorAll('#construction .hidden-image').forEach(el => el.classList.add('show'));
+//   this.style.display = 'none';
+// });
